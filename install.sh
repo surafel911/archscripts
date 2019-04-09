77db4fee551d4b94b07df70ef45fa3cc0ba02e1e
Surafel Assefa <surafel911@gmail.com>
2019-04-09 18:08:27 -0400

    Adds installation script.

---
+++ install.sh
@@ -0,0 +1,10 @@
+mkdir $HOME/.archscripts/
+
+sudo cp mkbuild /usr/local/bin/
+sudo cp mkbootusb /usr/local/bin/
+sudo cp aurlook /usr/local/bin/
+sudo cp aursearch /usr/local/bin/
+sudo cp aurinstall /usr/local/bin/
+sudo cp pacsearch /usr/local/bin/
+sudo cp pacupdate /usr/local/bin/
+sudo cp pacinstall /usr/local/bin/
