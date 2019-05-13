308675a88da0e3656a56e77dab85bccbf1790c91
Surafel Assefa <surafel911@gmail.com>
2019-05-13 18:32:06 -0400

    Adds git-clone script to obfuscate repetition.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ mkdir $HOME/.archscripts/
 
 sudo cp mkbuild /usr/local/bin/
 sudo cp mkbootusb /usr/local/bin/
+sudo cp git-clone /usr/local/bin
 sudo cp aurlook /usr/local/bin/
 sudo cp aursearch /usr/local/bin/
 sudo cp aurinstall /usr/local/bin/
@@ -9,4 +10,5 @@ sudo cp pacsearch /usr/local/bin/
 sudo cp pacupdate /usr/local/bin/
 sudo cp pacinstall /usr/local/bin/
 
-echo "Create a default `Makefile.tmp` file in /usr/local/etc/."
+echo "..."
+echo "Create a default 'Makefile.tmp' file in /usr/local/etc/."
